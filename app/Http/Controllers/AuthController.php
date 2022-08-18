30fd63becd00958186b529ed59ddc21b54942f3a
fandiadhitya <fandiadhitya96@gmail.com>
2022-08-18 15:59:20 +0700

    init

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller {
+  
+  public function auth(Request $request) {
+    
+    $this->validate($request, [
+      'username' => 'required',
+      'password' => 'required'
+    ]);
+    
+    $username = $request->input('username');
+    $password = $request->input('password');
+
+    $user = User::where('username', $username)->first();
+
+    if(!$user) {
+      $output = [
+        'message' => "User tidak ditemukan"
+      ];
+      return response()->json($output, 404);
+    }
+
+    $isPasswordValid = Hash::check($password, $user->password);
+
+    if(!$isPasswordValid){
+      $output = [
+        'message' => "Username & password salah!"
+      ];
+      return response()->json($output, 422);
+    }
+    
+    $generateToken = bin2hex(random_bytes(40));
+
+    $user->update([
+      'token' => $generateToken
+    ]);
+
+    return response()->json($user);
+  }
+
+}
